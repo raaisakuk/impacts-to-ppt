cf4fe2d9470a8819e685f7cecf14538719fe3704
Raaisa <raaisa.iitd@gmail.com>
2018-01-28 23:28:23 -0500

    Remove files shifted to module

--- pedsim.py
+++
@@ -1,9 +0,0 @@
-import constants
-
-def calc_score_binary(df, header_list):
-    """"yes/no scores
-    """
-    subdf = df[[header_list]]
-    score = subdf.iloc[0, :].value_counts(True).loc["Yes"]
-    return score
-
