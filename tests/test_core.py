616cbd3bf0185ee06bd1974cde978268d49b67ce
Raaisa <raaisa.iitd@gmail.com>
2018-02-24 22:50:03 -0500

    Update tests
    Added .p files and updated tests as per changes in the funcs cal
    case performance data and score

--- tests/test_core.py
+++ tests/test_core.py
@@ -6,9 +6,11 @@ import pedsim.core as core
 import pedsim.constants as const
 
 path_dir = os.path.join(os.path.dirname(__file__))
-two_rows_df = pickle.load(open(os.path.join(path_dir,"hn.p"),"r"))
-single_row_df = pickle.load(open(os.path.join(path_dir,"xb.p"),"r"))
-fbd_df = pickle.load(open(os.path.join(path_dir,"hn_fbd.p"),"r"))
+two_rows_df = pickle.load(open(os.path.join(path_dir, "hn.p"), "r"))
+single_row_df = pickle.load(open(os.path.join(path_dir, "xb.p"), "r"))
+fbd_df = pickle.load(open(os.path.join(path_dir, "hn_fbd.p"), "r"))
+empty_fbd_case = pickle.load(open(os.path.join(path_dir, "empty_fbd_case.p"), "r"))
+fbd_all_no = pickle.load(open(os.path.join(path_dir, "fbd_all_no.p"), "r"))
 #pickle.dump(xb,open("xb.p","w"))
 
 def test_get_hospital_data_two_rows():
@@ -18,7 +20,7 @@ def test_get_hospital_data_two_rows():
 
 def test_get_hospital_data_single_row():
     input_file = os.path.join(path_dir, "simulated_data_automation.xlsx")
-    xb = core.get_hospital_data(input_file,"xb")
+    xb = core.get_hospital_data(input_file, "xb")
     assert pd.DataFrame.equals(xb, single_row_df)
 
 def test_get_case_performance_data():
@@ -26,7 +28,13 @@ def test_get_case_performance_data():
     assert pd.DataFrame.equals(case_df, fbd_df)
 
 def test_get_case_performance_score():
-    assert core.get_case_performance_score(fbd_df, const.foreign_body_case) == 50
+    assert core.get_case_performance_score(fbd_df) == 50
+
+def test_get_case_performance_score_all_nan():
+    assert core.get_case_performance_score(empty_fbd_case) == -1
+
+def test_get_case_performance_score_all_no():
+    assert core.get_case_performance_score(fbd_all_no) == 0
 
 def test_get_case_performance_graph():
     core.get_case_performance_graph('Garfield', 'fbd', 50, 'test_plot.png')
