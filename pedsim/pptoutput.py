f57aef77f8dee6e546d104979707989cf7a03562
Raaisa <raaisa.iitd@gmail.com>
2018-03-03 18:12:28 -0500

    Add tables and figures to ppt

--- pedsim/pptoutput.py
+++ pedsim/pptoutput.py
@@ -36,15 +36,14 @@ output to a PowerPoint file.
 '''
 
 from __future__ import print_function
+import os
+
+import pandas as pd
 from pptx import Presentation
 from pptx.util import Inches
-import argparse
-import pandas as pd
-import numpy as np
-from datetime import date
-import matplotlib.pyplot as plt
-import seaborn as sns
+from pptx.util import Pt
 
+path_dir = os.path.join(os.path.dirname(__file__))
 
 def df_to_table(slide, df, left, top, width, height, colnames=None):
     """Converts a Pandas DataFrame to a PowerPoint table on the given
@@ -78,32 +77,70 @@ def df_to_table(slide, df, left, top, width, height, colnames=None):
             val = m[row, col]
             text = str(val)
             res.table.cell(row + 1, col).text = text
+            para = res.table.cell(row + 1, col).text_frame.paragraphs[0]
+            para.font.size = Pt(12)
 
-
-def create_ppt(input, output, report_data, chart):
+def create_ppt(output, case_figs, case_tables, perf_fig, emsc_fig, emsc_parts_fig, overall_table):
     """ Take the input powerpoint file and use it as the template for the output
     file.
     """
-    prs = Presentation(input)
-    # Use the output from analyze_ppt to understand which layouts and placeholders
-    # to use
-    # Create a title slide first
-    title_slide_layout = prs.slide_layouts[0]
-    slide = prs.slides.add_slide(title_slide_layout)
-    left = top = Inches(1)
-    pic = slide.shapes.add_picture(chart, left, top)
+    prs = Presentation(os.path.join(path_dir, 'report_out.pptx'))
 
-    slide = prs.slides.add_slide(prs.slide_layouts[2])
-    top = Inches(1.5)
-    left = Inches(0.25)
-    width = Inches(9.25)
+    #Slide 1
+    perf_summ = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(perf_summ)
+    perf_fig.savefig('perf_fig.png')
+    top = Inches(1)
+    left = Inches(1)
+    width = Inches(10)
+    height = Inches(5)
+    slide.shapes.add_picture('perf_fig.png', left, top, width, height)
+
+    #Slide 2
+    emsc = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(emsc)
+    emsc_fig.savefig('emsc_fig.png')
+    top = Inches(1)
+    left = Inches(1)
+    width = Inches(3)
+    height = Inches(3)
+    slide.shapes.add_picture('emsc_fig.png', left, top, width, height)
+
+    emsc_parts_fig.savefig('emsc_parts_fig.png')
+    top = Inches(1)
+    left = Inches(5)
+    width = Inches(7)
+    height = Inches(5)
+    slide.shapes.add_picture('emsc_parts_fig.png', left, top, width, height)
+
+    #Slide 3
+    for i in range(len(case_figs)):
+        if isinstance(case_tables[i], pd.DataFrame):
+            case_slide = prs.slide_layouts[0]
+            slide = prs.slides.add_slide(case_slide)
+            top = Inches(1)
+            left = Inches(5)
+            width = Inches(5.0)
+            height = Inches(5.0)
+            df_to_table(slide, case_tables[i], left, top, width, height)
+
+            case_figs[i].savefig('case_'+str(i)+'.png')
+            top = Inches(1)
+            left = Inches(1)
+            width = Inches(3)
+            height = Inches(3)
+            slide.shapes.add_picture('case_0.png', left, top, width, height)
+
+    #Slide 4
+    overall_perf = prs.slide_layouts[0]
+    slide = prs.slides.add_slide(overall_perf)
+    top = Inches(1)
+    left = Inches(1)
+    width = Inches(5.0)
     height = Inches(5.0)
-    # Flatten the pivot table by resetting the index
-    # Create a table on the slide
-    df_to_table(slide, report_data, left, top, width, height)
+    df_to_table(slide, overall_table, left, top, width, height)
+
     prs.save(output)
 
 
-if __name__ == "__main__":
-    df = pd.read_csv('fbd.csv')
-    create_ppt('report_out.pptx', 'trial.pptx', df, "emsc.png")
+
