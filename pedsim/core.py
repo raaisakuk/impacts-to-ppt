cce6d9faeae6e28f03adbc7d869b9037b2341f13
r_maya <raghuram1869@gmail.com>
2018-08-19 12:04:07 +0530

    [Add]: add and modify functions to accommodate validation and visuals
    Following features were accommodated:
    1. team particiption validation
    2. ged and ped automation with timestamp
    3. generation of new excel file and updated date
    4. Initialising the excel sheet that is being used for the first time
    5. Replace long questions with shorter values in ppt-tables
    6. Error messsages with accurate column names
    7. Correct the logic in calculation of total emsc score
    8. Use matplotlib instead of pygal for the radar graph
    9. Introduce xlabels in radar graph
    10. Change the heading of last table from metric to other elements

--- pedsim/core.py
+++ pedsim/core.py
@@ -2,11 +2,10 @@ from __future__ import division
 from math import pi
 import re
 
-from pygal.style import Style
+import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import pygal as pygal
 import seaborn as sns
 
 import constants as const
@@ -26,7 +25,43 @@ def get_hospital_data(excel_file, hospital_name):
     report_out = pd.read_excel(excel_file)
     new_report_out = report_out[report_out[const.site_hosp] == hospital_name].\
         groupby([const.site_hosp]).max().fillna('').reset_index()
-    return new_report_out.replace('', np.nan)
+    try:
+        latest_timestamp = max(report_out[[not x for x in report_out[const.timestamp].\
+            isna()]][const.timestamp])
+    except KeyError:
+        report_out = initialise_df(report_out)
+        latest_timestamp = max(report_out[const.timestamp])
+    if latest_timestamp==latest_timestamp:
+        latest = report_out[report_out[const.timestamp]==latest_timestamp].reset_index()
+        for each in const.ged_score.keys():
+            const.ged_score[each] = latest[each][0]
+        for each in const.ped_score.keys():
+            const.ped_score[each] = latest[each][0]
+        const.total_ged_count = latest['ged_count'][0]
+        const.total_ped_count = latest['ped_count'][0]
+    return (report_out, new_report_out.replace('', np.nan))
+
+def initialise_df(df):
+    empty_data_column = [np.nan for i in range(len(df))]
+    extra_columns = ["ped_count", 'ged_count', "timestamp", "ged_fbd",\
+     "ged_sepsis", "ged_seizure", "ged_cardiac_arrest", "ged_teamwork",\
+     "ged_emsc", "ged_qipi", "ged_emsc_staff", "ged_emsc_safety", "ged_emsc_equip",\
+     "ged_emsc_policy", "ped_fbd", "ped_sepsis", "ped_seizure", "ped_cardiac_arrest",\
+     "ped_teamwork", "ped_emsc", "ped_qipi", "ped_emsc_staff", "ped_emsc_safety",\
+     "ped_emsc_equip", "ped_emsc_policy"]
+    for each_col in extra_columns:
+        df[each_col] = empty_data_column
+    return df
+
+def validate_team_participation(hosp_df, case_header):
+    validate_series = hosp_df.filter(like=case_header[0]).T.reset_index()[0].isna()
+    for question in case_header:
+        case_df = hosp_df.filter(like=question).T.reset_index()
+        case_df['index'] = case_df['index'].apply(lambda x: re.split('[.][0-9]$', x)[0])
+        case_df = case_df[case_df['index'].isin(case_header)].reset_index()
+        this_series = case_df[0].isna()
+        if (validate_series != this_series).any():
+            raise ValueError(const.case_error_message+question)
 
 def get_case_performance_data(hosp_df, case_headers):
     '''Getting data for all the teams for a particular set of questions. Here we are
@@ -87,35 +122,19 @@ def get_case_performance_checklist(case_df):
     else:
         return np.nan
 
-def get_case_performance_graph(hosp_name, case_name, case_score):
-    '''Plot bar graph for case performance score with GED and PED scores
-    :param hosp_name: Name of the hospital
-    :param case_name: Name of the case to get ged and ped score from constants
-    :param case_score: Score for the case
-    :return: Figure handle for the bar plot
-    '''
-    sns.set_style('whitegrid')
-    fig, ax = plt.subplots(figsize=(10, 5))
-    pos = [0, 1, 2]
-    width = 0.25
-    y_vals = [const.ged_score[case_name], const.ped_score[case_name], case_score]
-    x_vals = [const.ged_name, const.ped_name, hosp_name]
-    plt.bar(pos, y_vals, width, align='center', alpha=0.5, color=sns.color_palette("muted"))
-    plt.xticks(pos, x_vals)
-    ax.set_xticklabels(x_vals, rotation="vertical")
-    plt.ylabel('%')
-    plt.title('Case Performance| '+const.case_name_dict[case_name])
-    plt.ylim([0, 100])
-    for a, b in zip(pos, y_vals):
-        ax.text(a, b+0.25, str(b), color='blue', fontweight='bold')
-    plt.tight_layout()
-    return fig
+def get_updated_checklist_format(checklist_df, case_name):
+    case_replacement = const.case_map[case_name]
+    replacements = []
+    for each in checklist_df[const.index_name]:
+        replacements.append(case_replacement[each])
+    checklist_df[const.index_name] = replacements
+    return checklist_df
 
 def get_case_performance_graph_donut(hosp_name, case_name, case_score):
     hosp_graph = [[100 - case_score, case_score], ['white', const.hosp_color], 1]
-    ped_graph = [[100 - const.ped_score[case_name], const.ped_score[case_name]], 
+    ped_graph = [[100 - const.ped_score["ped_"+case_name], const.ped_score["ped_"+case_name]],
                     ['white', const.ped_color], 1.15]
-    ged_graph = [[100 - const.ged_score[case_name], const.ged_score[case_name]], 
+    ged_graph = [[100 - const.ged_score["ged_"+case_name], const.ged_score["ged_"+case_name]],
                     ['white', const.ged_color], 1.3]
     center_graph = [[100], ['white'], 0.85]
 
@@ -125,10 +144,10 @@ def get_case_performance_graph_donut(hosp_name, case_name, case_score):
             startangle=90, shadow=False, radius=each[2])
     plt.text(0, 0.1, str(case_score)+"%", horizontalalignment='center',
          verticalalignment='center', fontsize=80, weight='bold', color='#00BFFF')
-    plt.text(0,-0.14, "PED-"+str(const.ped_score[case_name])+"%", 
-         horizontalalignment='center', verticalalignment='center', fontsize=40, 
+    plt.text(0,-0.14, "PED-"+str(const.ped_score["ped_"+case_name])+"%", 
+         horizontalalignment='center', verticalalignment='center', fontsize=40,
          weight='bold', color='#E3E3E3')
-    plt.text(0,-0.34, "GED-"+str(const.ged_score[case_name])+"%",
+    plt.text(0,-0.34, "GED-"+str(const.ged_score["ged_"+case_name])+"%",
          horizontalalignment='center', verticalalignment='center', fontsize=40,
          weight='bold', color='#03A89E')
     return fig
@@ -146,12 +165,15 @@ def create_case_df_fig(hosp_name, hosp_df, case_header, case_name):
     constants
     :return: case checklist, case performance fig, case score
     '''
+    validate_team_participation(hosp_df, case_header)
     data = get_case_performance_data(hosp_df, case_header)
     df = get_case_performance_checklist(data)
     score = round(get_case_performance_score(data), 2)
     if np.isnan(score):
         return df, np.nan, score
     fig = get_case_performance_graph_donut(hosp_name, case_name, score)
+    if isinstance(df, pd.DataFrame):
+        df = get_updated_checklist_format(df, case_name)
     return df, fig, score
 
 def get_emsc_score(hosp_df, emsc_header, weights):
@@ -166,6 +188,10 @@ def get_emsc_score(hosp_df, emsc_header, weights):
     order
     :return: percentage score which will be nan if all vals nan
     '''
+    is_entry_na = hosp_df[emsc_header].isna().any()
+    for i in range(len(emsc_header)):
+        if is_entry_na[i]==True:
+            raise ValueError("Following column cannot be empty:\n\t"+emsc_header[i])
     hosp_val_df = utils.convert_truth_values_to_num(hosp_df)
     total_score = sum(weights)
     num = 0
@@ -174,12 +200,21 @@ def get_emsc_score(hosp_df, emsc_header, weights):
     percent_score = 100*np.around(num/total_score, decimals=2)
     return percent_score
 
-def get_total_emsc_score(qipi, staff, safety, equip, policy, admin):
+def get_total_emsc_score(qipi, admin, staff, safety, policy, equip):
     '''Total EMSC Readiness score is average of all the EMSC case scores
     calculated separately. As the scores are in percentages, final result
     is a percentage as well
     '''
-    return np.around((qipi+staff+safety+equip+policy+admin)/6, decimals=2)
+    total_emsc_weights = []
+    numerator = 0
+    weights = [const.qi_pi_score, const.score_admin, const.score_staff,
+                 const.safety_score, const.policy_score, const.equip_score]
+    percentages=[qipi, admin, staff, safety, policy, equip]
+    for i in range(len(percentages)):
+        numerator+=percentages[i]*sum(weights[i])
+        total_emsc_weights.extend(weights[i])
+
+    return np.around(numerator/sum(total_emsc_weights), decimals=2)
 
 def plot_triple_bargraph(first_name, first_val_arr, second_name,
                          second_val_arr, third_name, third_val_arr, ylabel,
@@ -220,15 +255,45 @@ def plot_triple_bargraph(first_name, first_val_arr, second_name,
 def plot_triple_radargraph(first_name, first_val_arr, second_name,
                          second_val_arr, third_name, third_val_arr,
                          title, xlabels):
+    N = len(xlabels)
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+    fig = plt.figure(figsize=(30,15))
+    ax = fig.add_subplot(111, polar=True)
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+    ax.set_title(title, size=45)
+    ax.set_rlabel_position(0)
+    plt.yticks([10 * i for i in range(1,10)],
+        [10 * i for i in range(1,10)],
+        color="black", size=20)
+    plt.ylim(0,100)
+    names = [second_name, third_name, first_name]
+    val_arr = [second_val_arr, third_val_arr, first_val_arr]
+    colors = [const.ped_color, const.ged_color, const.hosp_color]
+    for i in range(len(names)):
+        val_arr[i] += val_arr[i][:1]
+        ax.plot(angles, val_arr[i], linewidth=1, linestyle='solid',
+         label=names[i], color=colors[i])
+        ax.fill(angles, val_arr[i], colors[i], alpha=0.7)
+    plt.xticks(angles[:-1],[])
+    for i in range(N):
+        angle_rad = i / float(N) * 2 * pi
+        if angle_rad == 0:
+            ha, distance_ax = "center", 2
+        elif 0 < angle_rad < pi:
+            ha, distance_ax = "left", 2
+        elif angle_rad == pi:
+            ha, distance_ax = "center", 3
+        else:
+            ha, distance_ax = "right", 2
+        ax.text(angle_rad, 100 + distance_ax,
+            xlabels[i]+": "+str(int(first_val_arr[i]))+"%", size=25, color = "#1874CD",
+            horizontalalignment=ha, verticalalignment="center")
 
-    custom_style = Style(colors = (const.ped_color, const.ged_color, const.hosp_color))
-    radar_chart = pygal.Radar(fill=True, width=800, height=400, style=custom_style)
-    radar_chart.title = title
-    radar_chart.x_labels = xlabels
-    radar_chart.add(second_name, first_val_arr)
-    radar_chart.add(third_name, second_val_arr)
-    radar_chart.add(first_name, third_val_arr)
-    radar_chart.render_to_png('perf_fig.png')
+    plt.legend(loc='upper right', bbox_to_anchor=(0.1, 0.1), prop={'size':20})
+    return fig
+
 
 def plot_emsc_graph(hosp_name, qipi, staff, safety, equip, policy):
     '''Plot EMSC graph with hospital, GED and PED scores for all EMSC
@@ -241,12 +306,12 @@ def plot_emsc_graph(hosp_name, qipi, staff, safety, equip, policy):
     :param policy: Policies/Procedures score
     :return: Figure handle
     '''
-    ged_scores = [const.ged_score["emsc_qipi"], const.ged_score["emsc_policy"],
-                  const.ged_score["emsc_safety"], const.ged_score["emsc_staff"],
-                  const.ged_score["emsc_equip"]]
-    ped_scores = [const.ped_score["emsc_qipi"], const.ped_score["emsc_policy"],
-                  const.ped_score["emsc_safety"], const.ped_score["emsc_staff"],
-                  const.ped_score["emsc_equip"]]
+    ged_scores = [const.ged_score["ged_emsc_qipi"], const.ged_score["ged_emsc_policy"],
+                  const.ged_score["ged_emsc_safety"], const.ged_score["ged_emsc_staff"],
+                  const.ged_score["ged_emsc_equip"]]
+    ped_scores = [const.ped_score["ped_emsc_qipi"], const.ped_score["ped_emsc_policy"],
+                  const.ped_score["ped_emsc_safety"], const.ped_score["ped_emsc_staff"],
+                  const.ped_score["ped_emsc_equip"]]
     hosp_scores = [qipi, policy, safety, staff, equip]
     return plot_triple_bargraph(hosp_name, hosp_scores, const.ped_name, ped_scores,
                          const.ged_name, ged_scores, "Score %", "EMSC Pediatric Readiness",
@@ -266,12 +331,12 @@ def plot_performance_summary(hosp_name, fbd, sepsis, cardiac_arrest, teamwork, e
     :param seizure: Seizure Case Score
     :return: Figure handle
     '''
-    ged_scores = [const.ged_score["emsc"], const.ged_score["fbd"], const.ged_score["sepsis"],
-                  const.ged_score["seizure"], const.ged_score["cardiac_arrest"],
-                  const.ged_score["teamwork"]]
-    ped_scores = [const.ped_score["emsc"], const.ped_score["fbd"], const.ped_score["sepsis"],
-                  const.ped_score["seizure"], const.ped_score["cardiac_arrest"],
-                  const.ped_score["teamwork"]]
+    ged_scores = [const.ged_score["ged_emsc"], const.ged_score["ged_fbd"], const.ged_score["ged_sepsis"],
+                  const.ged_score["ged_seizure"], const.ged_score["ged_cardiac_arrest"],
+                  const.ged_score["ged_teamwork"]]
+    ped_scores = [const.ped_score["ped_emsc"], const.ped_score["ped_fbd"], const.ped_score["ped_sepsis"],
+                  const.ped_score["ped_seizure"], const.ped_score["ped_cardiac_arrest"],
+                  const.ped_score["ped_teamwork"]]
     hosp_scores = [emsc, fbd, sepsis, seizure, cardiac_arrest, teamwork]
     xlabels = ["EMSC Readiness", "Foreign Body", "Sepsis",
                     "Seizure", "Cardiac Arrest", "Teamwork"]
@@ -285,8 +350,7 @@ def plot_performance_summary(hosp_name, fbd, sepsis, cardiac_arrest, teamwork, e
     ged_scores = ged_scores[6:]
     ped_scores = ped_scores[6:]
     xlabels = xlabels[6:]
-    
-    plot_triple_radargraph(hosp_name, hosp_scores, const.ped_name, ped_scores, const.ged_name,
+    return plot_triple_radargraph(hosp_name, hosp_scores, const.ped_name, ped_scores, const.ged_name,
                                 ged_scores, "Performance Summary", xlabels)
 
 def get_cts_score(hosp_df, header):
@@ -311,14 +375,10 @@ def get_cts_ind_score(hosp_df, cts_ind_header):
     '''
     curr_df = get_case_performance_data(hosp_df, cts_ind_header)
     if isinstance(curr_df, pd.DataFrame):
-        print(curr_df[map(lambda x: not x, curr_df[const.hosp_ans].isnull())])
         curr_df = curr_df.astype(dtype= {const.hosp_ans:"float64"})
-        percent_score = 100*np.around(curr_df[const.hosp_ans].sum()/(10*curr_df[const.hosp_ans].count()),
-                                      decimals=2)
-        print(curr_df[const.hosp_ans].sum(), curr_df[const.hosp_ans].count(), percent_score)
+        percent_score = 100*np.around(curr_df[const.hosp_ans].\
+            sum()/(10*curr_df[const.hosp_ans].count()),decimals=2)
     else:
-        # print(cts_ind_header[0])
-        # print("\n\n")
         percent_score = np.nan
     return percent_score
 
@@ -334,7 +394,6 @@ def get_cts_score_from_parts(hosp_df):
     fbd = get_cts_ind_score(hosp_df, const.cts_tool_ind_fbd)
     seiz = get_cts_ind_score(hosp_df, const.cts_tool_ind_seiz)
     sep = get_cts_ind_score(hosp_df, const.cts_tool_ind_sep)
-    print("Casewise - ",fbd, sep, seiz, cardiac)
     return np.around(np.nanmean([cardiac, fbd, seiz, sep]), decimals=2)
 
 def get_overall_performance_scores(hosp_df):
@@ -348,10 +407,6 @@ def get_overall_performance_scores(hosp_df):
     disposition = get_case_performance_data(hosp_df, const.disposition)
     fam_pre = get_case_performance_data(hosp_df, const.family_pres)
     fam_care = get_case_performance_data(hosp_df, const.family_care)
-    print(weight_data)
-    print(disposition)
-    print(fam_pre)
-    print(fam_care)
     cts_score = get_cts_score(hosp_df, const.cts_tool_all)
     if np.isnan(cts_score):
        cts_score = get_cts_score_from_parts(hosp_df)
@@ -369,5 +424,24 @@ def create_overall_df(overall_scores_dict):
     :return: dataframe with columns Metric and Score
     '''
     df = pd.DataFrame(list(overall_scores_dict.iteritems()),
-                        columns=['Metric', 'Score'])
+                        columns=['Other Elements', 'Score'])
     return df
+
+def update_average_scores(total_df, excel_file, hosp_name, this_scores):
+    new_filepath = (".".join(excel_file.split(".")[:-1])).split("___")[0] +\
+        "___"+"-".join(re.split("\s|\:|\.", str(dt.datetime.now())))+".xlsx"
+    for each_key in this_scores.keys():
+        updated_ped_score = (const.ped_score['ped_'+each_key]*const.total_ped_count+
+            this_scores[each_key])/(const.total_ped_count+1)
+        updated_ged_score = (const.ged_score['ged_'+each_key]*const.total_ged_count+
+            this_scores[each_key])/(const.total_ged_count+1)
+        total_df.loc[total_df[const.site_hosp]==hosp_name, 'ped_'+each_key] = updated_ped_score
+        total_df.loc[total_df[const.site_hosp]==hosp_name, 'ged_'+each_key] = updated_ged_score
+    total_df.loc[total_df[const.site_hosp]==hosp_name, 'ped_count']\
+        = const.total_ped_count+1
+    total_df.loc[total_df[const.site_hosp]==hosp_name, 'ged_count']\
+        = const.total_ged_count+1
+    total_df.loc[total_df[const.site_hosp]==hosp_name, const.timestamp] = dt.datetime.now()
+    writer = pd.ExcelWriter(new_filepath, engine='openpyxl')
+    total_df.to_excel(writer)
+    writer.save()
